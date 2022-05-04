aedc624be61d4fb59378953a87571fe494117c5b
Danielopez1102 <daniel.espinoza.lopez@academiacuatropuntocero.com>
2022-05-03 21:40:42 -0600

    mi trabajo 2-5

---
+++ index.js
@@ -0,0 +1,35 @@
+alerta ('Esta funcionando');
+
+// 1
+var edad = 19;
+
+if ((edad>=18)||(edad>60)) {
+ consola. log('me compraron una galleta');
+}else{
+ consola. log('no me compraron una galleta');
+}
+
+// 2
+var perro = verdadero;
+ var gato = verdadero;
+
+if ((perro = true)||(gato = true)) { 
+ consola. log('entoces estoy feliz'); 
+}else{
+ consola. log('estoy triste');
+}
+
+// 3
+var numero_inicial = 1
+
+ function contador_de_numeros_hasta_el_100(numero_inicial) {
+    while (numero_inicial<=100) {
+    if (numero_inicial%3==1) {
+        console.log(numero_inicial);
+    }
+    numero_inicial = numero_inicial+3;
+
+    }
+}
+
+contador_de_numeros_hasta_el_100(numero_inicial); 
